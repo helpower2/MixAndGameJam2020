af3339fdbdf35f539bcf24a44e2efe8b909a1a5b
Azhar Siddiqui <ozzie4391@gmail.com>
2020-11-14 03:22:13 +0500

    Added Isometric Movement in PlayerMovement scene

---
+++ Mix and game jam 2020/Assets/Scripts/Movement/IsometricMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricMovement : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+
+    Rigidbody2D rbody;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rbody.MovePosition(newPos);
+    }
+}
