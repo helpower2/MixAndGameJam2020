badf4b09292313e779cbfbb76efbe7e2adf449bf
ktejas <krishnasalimeda@gmail.com>
2020-11-14 11:16:34 +0530

    Added Game Scene with Player Movement, Art and Tilemap

---
+++ Mix and game jam 2020/Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+
+    Rigidbody2D rbody;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        if (movement.x > 0)
+            spriteRenderer.flipX = true;
+        else if(movement.x < 0)
+            spriteRenderer.flipX = false;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rbody.MovePosition(newPos);
+    }
+}
