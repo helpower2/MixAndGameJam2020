74f71e3aabc652049b3f84d64ac618419f1f9e02
rocleemusic <48lee@cardinalmail.cua.edu>
2020-11-15 12:01:46 -0500

    Finish Platform stops music

---
+++ Mix and game jam 2020/Assets/Scripts/FinishPlatform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FinishPlatform : MonoBehaviour
+{
+    [SerializeField] MusicManager musicManager;
+
+    private void Start()
+    {
+        musicManager = FindObjectOfType<MusicManager>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StopMusicOnFinishLevel();
+    }
+
+    void StopMusicOnFinishLevel()
+    {
+        if (musicManager != null)
+            musicManager.StopMusic();
+    }
+}
